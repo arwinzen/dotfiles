b7f2c6d1f51cda92a0c9e20e0882147654d6b564
Arwin Goo <arwingoo90@gmail.com>
2025-03-22 12:03:26 +0800

    fix daily note location

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -28,9 +28,9 @@ return {
     -- daily notes
      daily_notes = {
       -- Optional, if you keep daily notes in a separate directory.
-      folder = "0.Inbox/",
+      folder = "daily-notes/",
       -- Optional, if you want to change the date format for the ID of daily notes.
-      date_format = "%Y.%M.%D",
+      date_format = "%Y.%m.%d",
       -- Optional, if you want to change the date format of the default alias of daily notes.
       alias_format = "%B %-d, %Y",
       -- Optional, default tags to add to each new daily note created.
